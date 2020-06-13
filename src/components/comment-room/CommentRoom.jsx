ae422b7f95c49c7e10c4c9af4cd2a6cca6af2cfa
Justice Appiah <jaxtis.apia@gmail.com>
2020-06-13 10:29:07 +0000

    component[comment-room]: add switching loading ui

--- src/components/comment-room/CommentRoom.jsx
+++ src/components/comment-room/CommentRoom.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import CommentReply from './CommentReply'
 import Comment from './Comment'
+import { Dimmer, Loader } from 'semantic-ui-react'
 
-const CommentRoom = ({ comments = [], postId, onCommentReply, onPostReply }) => {
+const CommentRoom = ({ comments = [], postId, onCommentReply, onPostReply, isCommentsLoading }) => {
     return (
-        <div class="comment-room">
+        <div className="comment-room">
+            <Dimmer active={isCommentsLoading} inverted>
+                <Loader>Loading Comments</Loader>
+            </Dimmer>
+
             <div className="comment-room__comments">
                 {comments.map(comment => <Comment key={comment.id} {...comment} onReplySubmitted={onCommentReply} />)}
             </div>
 
-            <div class={'comment-room__reply-form'}>
+            <div className={'comment-room__reply-form'}>
                 <CommentReply postId={postId} onReplySubmitted={onPostReply} />
             </div>
         </div>
